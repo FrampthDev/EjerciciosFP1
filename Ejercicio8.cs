30c0f389eeefc2f82f30bd0e553f98a7767c0c15
link10804k <62154757+link10804k@users.noreply.github.com>
2024-10-01 12:19:43 +0200

    Ejercicios 7 y 8 actualizados

---
+++ Ejercicio8.cs
@@ -0,0 +1,44 @@
+﻿// Javier Zazo Morillo
+// Miguel Ángel González López
+
+using System.Drawing;
+
+namespace Ejercico_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool triangle, equilateral, isosceles, scalene, rectangle;
+            int l1, l2, l3;
+
+            Console.WriteLine("Lado 1: ");
+            l1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lado 2: ");
+            l2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lado 3: ");
+            l3 = int.Parse(Console.ReadLine());
+
+            triangle = ((l1 > 0) && (l2 > 0) && (l3 > 0)) && ((l1 < l2 + l3) && (l2 < l1 + l3) && (l3 < l1 + l2));
+
+            equilateral = triangle && (l1 == l2 && l1 == l3);
+
+            isosceles = triangle && ((l1 == l2 && l1 != l3) 
+                || (l1 == l3 && l1 != l2) 
+                || (l2 == l3 && l2 != l1));
+
+            scalene = triangle && (l1 != l2 && l1 != l3 && l2 != l3);
+
+            rectangle = triangle && ((l1 * l1 == l2 * l2 + l3 * l3)
+                || (l2 * l2 == l1 * l1 + l3 * l3)
+                || (l3 * l3 == l1 * l1 + l2 * l2));
+
+            Console.WriteLine("Triangulo: " + triangle);
+            Console.WriteLine("Equilátero: " + equilateral);
+            Console.WriteLine("Isósceles: " + isosceles);
+            Console.WriteLine("Escaleno: " + scalene);
+            Console.WriteLine("Rectángulo: " + rectangle);
+
+        }
+    }
+}
