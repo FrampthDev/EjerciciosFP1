1428bca977ef7d38ae9f0f1413392bec44082f01
frampth <frampth@proton.me>
2024-10-08 18:01:01 +0200

    Ejercicio 1 Hoja 3

---
+++ Hoja3/Ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Timers;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Ingrese el numero entero a factorizar: ");
+
+            int n = int.Parse(Console.ReadLine());
+            int cont=1, f = 1;
+
+
+            while (cont<(n+1)){
+                
+                f = f*cont;
+                cont +=1;
+                
+
+            }
+
+            Console.WriteLine("El numero factorizado es: "+ f);
+    }
+    }
+}
