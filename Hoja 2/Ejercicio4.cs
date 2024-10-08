a3be25c3813255a9f72181b2b8075e3ab916a4fa
link10804k <62154757+link10804k@users.noreply.github.com>
2024-10-08 09:41:38 +0200

    3 Ejercicios más

---
+++ Hoja 2/Ejercicio4.cs
@@ -0,0 +1,37 @@
+﻿namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int d1, d2, d3, d4, n;
+            bool one3, two3, twocon3, cap;
+
+            Console.WriteLine("Introduzca un número de 4 dígitos: ");
+            n = int.Parse(Console.ReadLine());
+
+            d4 = n / 1000;
+            d3 = n % 1000 / 100;
+            d2 = n % 100 / 10;
+            d1 = n % 10;
+
+            one3 = ((d4 == 3) || (d3 == 3) || (d2 == 3) || (d1 == 3));
+
+            two3 = (((d4 == 3) && (d3 == 3)) || ((d4 == 3) && (d2 == 3)) || ((d4 == 3) && (d1 == 3))
+                || ((d3 == 3) && (d2 == 3)) || ((d3 == 3) && (d1 == 3))
+                || ((d2 == 3) && (d1 == 3)));
+
+            twocon3 = (((d4 == 3) && (d3 == 3) && (d2 != 3)) 
+                || ((d4 != 3) && (d3 == 3) && (d2 == 3) && (d1 != 3)) 
+                || ((d3 != 3) && (d2 == 3) && (d1 == 3)));
+
+            cap = ((d4*1000+d3*100+d2*10+d1) == (d1+d2*10+d3*100+d4*1000));
+
+            Console.WriteLine("Algún 3: " + one3);
+            Console.WriteLine("Dos 3 o más : " + two3);
+            Console.WriteLine("Dos 3 seguidos: " + twocon3);
+            Console.WriteLine("Capicúa: " + cap); 
+
+        }
+    }
+}
---
+++ Hoja 2/Ejercicio7.cs
@@ -0,0 +1,28 @@
+﻿namespace Ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year, sup;
+            bool leap;
+            
+            Console.WriteLine("Introduzca un año: ");
+            year = int.Parse(Console.ReadLine());
+
+            sup = year / 4;
+
+            leap = (year == sup * 4);
+
+            sup = year / 100;
+
+            leap = (year != sup * 100);
+
+            sup = year / 400;
+
+            leap = leap || (year == sup * 400);
+
+            Console.WriteLine("El año es bisiesto: " + leap);
+    }
+}
+}
