45f2f1c29b145bd2ce3e0fc79cd93f5641469d37
frampth <frampth@proton.me>
2024-10-09 18:32:40 +0200

    Ejercicio 4 completado

--- Hoja4/Ejercicio4/Program.cs
+++ Hoja4/Ejercicio4/Program.cs
@@ -1,5 +1,28 @@
-﻿using Internal;
+﻿
 
-int number = int.Parse(Console.WriteLine);
-int reversedNumber;
+int number = int.Parse(Console.ReadLine());
+int cont=0;
 
+string reversedNumber="";
+int reversedNumberOutput=0;
+
+while (number>0){
+    string bufferNumber = Convert.ToString(number%10); 
+    reversedNumber = reversedNumber.Insert(cont, bufferNumber);
+    
+    number /=10;
+    cont ++;
+}
+
+reversedNumberOutput = Convert.ToInt16(reversedNumber);
+Console.WriteLine(reversedNumberOutput);
+
+
+
+
+/*string hola = "hola";
+int n = 3;
+
+string hola2= hola.Insert(3 , "2");
+
+Console.WriteLine(hola2);*/
