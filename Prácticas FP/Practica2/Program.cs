ae44988826f8594d0897028cad64dd23d70617b8
link10804k <62154757+link10804k@users.noreply.github.com>
2024-12-10 12:41:45 +0100

    final

--- Prácticas FP/Practica2/Program.cs
+++ Prácticas FP/Practica2/Program.cs
@@ -1,11 +1,7 @@
 ﻿// Javier Zazo Morillo
 // Miguel Ángel González López
 using System;
-using System.Data;
-using System.Diagnostics;
-using System.Globalization;
-using System.Security.Cryptography;
-using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace naves {
     class Program {
@@ -23,34 +19,60 @@ namespace naves {
 
             bool hayEnemigo = false; 
             bool hayBala = false;    
+            bool gameOver = false;
+            bool pause = false;
 
             int [] suelo = new int[ANCHO], // límites del tunel
                    techo = new int[ANCHO];
 
             int naveC = ANCHO / 2, naveF = ALTO / 2,   
                 balaC = 0, balaF = 0,  
-                enemigoC = 0, enemigoF = 0;
+                enemigoC = 0, enemigoF = 0,
+                colC = -1, colF = -1;
+                
 
             // inicializacion
             
     
             IniciaTunel(suelo, techo);
-            Render(suelo, techo, enemigoC, enemigoF,naveC,naveF);
+            Render(suelo, techo, enemigoC, enemigoF,naveC,naveF, balaC, balaF, hayBala, ref  colC, ref colF);
            
-            while (true){ // true provisional
-                AvanzaTunel(suelo, techo);
-                LogicaJuego(ref naveC, ref naveF, ref balaC, ref balaF, ref enemigoC, ref enemigoF, suelo, techo, ref hayEnemigo);
-                Render(suelo, techo, enemigoC, enemigoF,naveC,naveF);
+            while (!gameOver){ // true provisional
 
-                if (DEBUG == true) {
-                    Console.SetCursorPosition(0, ALTO);
-                    Console.Write($"Techo: [{techo[0]}] Suelo: [{suelo[0]}]"); // c
-                    Console.SetCursorPosition(0, ALTO + 1);
-                    Console.Write($"Posición enemigo: ({enemigoC},{enemigoF})");
-                    Console.WriteLine($"test: {naveC} | {naveF}");
+                char ch = LeeInput();
+
+                if (ch=='q'){
+                    gameOver = true;
                 }
+                else if (ch == 'p'){
+                    pause = true;
+                    while(pause == true){
+                        ch = LeeInput();
+                        if (ch == 'p'){
+                            pause = false;
+                        }
+                    }
+                }
+                else{
 
-                Thread.Sleep(120); 
+                    
+                    LogicaJuego(ch, ref gameOver, ref naveC, ref naveF, ref balaC, ref balaF, ref enemigoC, ref enemigoF, suelo, techo, ref hayEnemigo, ref hayBala, ref colC, ref colF);
+                    Render(suelo, techo, enemigoC, enemigoF,naveC,naveF, balaC, balaF, hayBala, ref colC, ref colF);
+
+                    if (DEBUG == true) {
+                        Console.SetCursorPosition(0, ALTO);
+                        Console.Write($"Techo: [{techo[0]}] Suelo: [{suelo[0]}]"); // 
+                        Console.SetCursorPosition(0, ALTO + 1);
+                        Console.Write($"Posición enemigo: ({enemigoC},{enemigoF})");
+                        Console.SetCursorPosition(0, ALTO + 2);
+                        Console.WriteLine($"Posición nave: ({naveC},{naveF})");
+                        Console.WriteLine($"Input: {ch}");
+                        Console.WriteLine($"GameState: {gameOver}");
+                    }
+
+                    Thread.Sleep(120);
+                }
+                 
             } // while
         } // Main
 
@@ -96,15 +118,27 @@ namespace naves {
 			}
             return ch;
         }
-        static void Render(int[] suelo, int[] techo, int enemigoC, int enemigoF, int naveC, int naveF) {
+        static void Render(int[] suelo, int[] techo, int enemigoC, int enemigoF, int naveC, int naveF, int balaC, int balaF, bool hayBala, ref int colC, ref int colF) {
+            
+            
+
             RenderTunel(suelo, techo);
             
-            if (enemigoC >0 ){
+            if (enemigoC > 0){
                 RenderizarEntidad(enemigoC,enemigoF,"<>");
             }
 
             if (!ColisionNave(naveC, naveF, suelo, techo, enemigoC, enemigoF)){
                 RenderizarEntidad(naveC,naveF,"=>");
+            }
+
+            if (hayBala && balaC >= 0) {
+                RenderizarEntidad(balaC,balaF, "--");
+            }
+            if (colC >0 && colF > 0){
+                RenderizarEntidad(colC, colF,"**");
+                colC = -1;
+                colF = -1;
             }
         }
         static void RenderTunel(int [] suelo, int [] techo){ //renderizado de la pantalla
@@ -125,9 +159,20 @@ namespace naves {
                 AvanzaTunel(suelo, techo);
             }
         }
-        static void LogicaJuego(ref int naveC, ref int naveF, ref int balaC, ref int balaF, ref int enemigoC,ref int enemigoF, int[] suelo, int[] techo, ref bool hayEnemigo){
+        static void LogicaJuego(char ch,ref bool gameOver,ref int naveC, ref int naveF, ref int balaC, ref int balaF, ref int enemigoC, ref int enemigoF, int[] suelo, int[] techo, ref bool hayEnemigo, ref bool hayBala, ref int colC, ref int colF){
+            
+            AvanzaTunel(suelo, techo);
             GeneraAvanzaEnemigo(ref enemigoC, ref enemigoF, ref hayEnemigo, suelo, techo);
-            AvanzaNave(LeeInput(),ref naveC,ref naveF, enemigoC, enemigoF, suelo, techo);
+            
+
+            if (ColisionNave(naveC, naveF, suelo, techo, enemigoC, enemigoF)) {gameOver = true;}
+            AvanzaNave(ref gameOver,ch,ref naveC,ref naveF, enemigoC, enemigoF, suelo, techo);
+            if (ColisionNave(naveC, naveF, suelo, techo, enemigoC, enemigoF)) {gameOver = true;}
+
+
+            ColisionBala(ref hayBala, ref balaC, balaF, ref enemigoC, enemigoF,ref hayEnemigo, suelo, techo, ref colC, ref colF);
+            GeneraAvanzaBala(ch, ref hayBala, ref balaC, ref balaF, naveC, naveF, enemigoC, enemigoF, suelo, techo);
+            ColisionBala(ref hayBala, ref balaC, balaF, ref enemigoC, enemigoF,ref hayEnemigo, suelo, techo, ref colC, ref colF);
         }
 
         static void GeneraAvanzaEnemigo(ref int enemigoC, ref int enemigoF, ref bool hayEnemigo, int[] suelo, int []techo){
@@ -144,7 +189,7 @@ namespace naves {
             else if(probabilidadDeGeneracion == 0){ // 1/4 de probabilidades de generar
                 enemigoC = ANCHO;
                 hayEnemigo = true;
-                enemigoF = filaAleatoria;  // ERROR
+                enemigoF = filaAleatoria;  
             }
             if (enemigoC <=-1){
                 hayEnemigo = false;
@@ -152,42 +197,40 @@ namespace naves {
             
         }
 
-        static void AvanzaNave(char ch, ref int naveC, ref int naveF, int enemigoC, int enemigoF, int[] suelo, int[] techo){
+        static void AvanzaNave(ref bool gameOver,char  ch, ref int naveC, ref int naveF, int enemigoC, int enemigoF, int[] suelo, int[] techo){
             
-
-            if ((naveF != suelo[naveF] && naveF != techo[naveF]) && (naveC != enemigoC && naveF!=enemigoF)){
+            
+            if ((naveF >= techo[naveC] && naveF <= suelo[naveC]) || (naveC != enemigoC && naveF != enemigoF)){
                 // Si la nave no está dentro del suelo, ni del techo ni del enemigo:
-
-
-                if (ch == 'l' && naveC >0){
+                
+                if (ch == 'l' && naveC > 0){
                     naveC--;
                 }
-                if (ch == 'r' && naveC <ANCHO-1){
+                else if (ch == 'r' && naveC < ANCHO-1){
                     naveC++;
                 }
-                if (ch == 'u' && naveF >0){
+                else if (ch == 'u' && naveF > 0){
                     naveF--;
                 }
-                if (ch == 'd' && naveF <ALTO-1){
+                else if (ch == 'd' && naveF < ALTO-1){
                     naveF++;
                 }
-            }
-
-            
+            }           
         }
-        static void GeneraAvanzaBala(bool hayBala, char ch, ref int balaC, ref int balaF, int naveC, int naveF, int enemigoC, int enemigoF, int [] suelo, int [] techo){
-            if (ch == 'x' && hayBala){
+        static void GeneraAvanzaBala(char ch, ref bool hayBala, ref int balaC, ref int balaF, int naveC, int naveF, int enemigoC, int enemigoF, int [] suelo, int [] techo){
+            if (ch == 'x' && !hayBala){
                 
                     // Genera una nueva bala por delante de la nave (naveC+1)
                     balaC = naveC +1;
                     balaF = naveF;
+                    hayBala = true;
             }
-            else if (hayBala && (balaF != techo[balaF] && balaF != techo[balaF]) && (balaF != enemigoF && balaC != enemigoC)){
+            else if (hayBala && (balaF != techo[balaC] && balaF != techo[balaC] && balaC < ANCHO - 1) && (balaF != enemigoF && balaC != enemigoC)){
 
                     // la bala avanza una posición
-                    balaC ++;
-            }           
-        
+                    balaC += 2;
+            } 
+            else hayBala = false;              
         }
 
         static void RenderizarEntidad(int entidadC, int entidadF, string sprite){
@@ -198,23 +241,61 @@ namespace naves {
         static bool ColisionNave(int naveC, int naveF, int [] suelo, int [] techo, int enemigoC, int enemigoF){
             bool colision = false;
 
-            /*if (naveF == suelo[naveF] || naveF == techo[naveF] || naveF == enemigoF && naveC == enemigoC){
-                // ERROR CORREGIR COLISIONES
+
+            if (naveF >= suelo[naveC] || naveF <= techo[naveC] || (naveF == enemigoF && naveC == enemigoC)){
                 colision = true;
                 
-            }*/
+            }
             
             // Determina si la nave colisiona contra el suelo, el techo o el enemigo.
             return colision;
         }
-        void ColisionBala(int balaC, int balaF, int enemigoC, int enemigoF, int[] suelo, int [] techo, int colC, int colF){
+        static void ColisionBala(ref bool hayBala, ref int balaC, int balaF, ref int enemigoC, int enemigoF,ref bool hayEnemigo ,int[] suelo, int [] techo, ref int colC, ref int colF){
             // si la bala está a la derecha de la pantalla la elimina
             //if (hayBala && )
 
 
+            if (balaC == enemigoC -1 && balaF == enemigoF){
+                colC = balaC + 1;
+                colF = balaF;
+                enemigoC = 0;
+                hayEnemigo = false;              
+                balaC = 0;
+                hayBala = false;
+            }
+            else if (balaC == enemigoC && balaF == enemigoF){
+                colC = balaC;
+                colF = balaF;
+                enemigoC = 0;
+                hayEnemigo = false;              
+                balaC = 0;
+                hayBala = false;
+            }
+
+            else if (balaC > 0 && balaF >= suelo[balaC - 1]){
+                suelo[balaC - 1] = balaF + 1;
+                balaC = 0;
+                hayBala = false;
+            }
+            else if (balaF >= suelo[balaC]){
+                suelo[balaC] = balaF + 1;
+                balaC = 0;
+                hayBala = false;
+            }
+            else if (balaC > 0 && balaF <= techo[balaC - 1]){  
+                techo[balaC - 1] = balaF - 1;
+                balaC = 0;
+                hayBala = false;
+            }     
+            else if (balaF <= techo[balaC]){  
+                techo[balaC] = balaF - 1;
+                balaC = 0;
+                hayBala = false;
+            }     
+            
             // si colisiona con el enemigo elimina la bala y el enemigo y devuelve en colC, colF la posición de la colisión
             // si colisiona con el suelo o con el techo elimina la bala y devuelve en colC y colF la posición de la colisión y destruye 3 bloques
-            //si no hay colisión simplemente devuelve colC=colF=-1 para indicarlo.
+            // si no hay colisión simplemente devuelve colC=colF=-1 para indicarlo.
         }
         }
     
